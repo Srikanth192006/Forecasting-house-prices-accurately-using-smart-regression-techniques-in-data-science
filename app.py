2a3c5d699beb89c196b4eb4d25ec6dd75298b7c0
Srikanth192006 <srikanthtemp06@gmail.com>
2025-05-15 21:40:20 +0530

    app.py

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
